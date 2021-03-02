1d5aa6f270531e9709131300b070b412150c6a68
Wojciech <kojtek41@wp.pl>
2021-03-02 11:36:10 +0100

    Zadania z pierwszej listy

--- main.py
+++ main.py
@@ -1,7 +1,86 @@
-import sys as system
-
-print(system.version)
-
-a = 4
-
-print(a+2)
+import cmath as m
+# zad1
+# liczba = 2
+# liczba2 = 4
+# napis = 'Witaj'
+# napis2 = 'Zmienna str'
+# prawda = True
+# fals = False
+# print(liczba)
+# print(liczba2)
+# print(napis)
+# print(napis2)
+# if prawda:
+#     print(napis)
+# zad2
+# a = input("Podaj liczbe: ")
+# b = input("Podaj druga liczbe: ")
+# a = float(a)
+# b = float(b)
+# print("a + b =", a + b)
+# print("a - b =", a - b)
+# print("a * b =", a * b)
+# print("a / b =", a / b)
+# print("a // b =", a // b)
+# print("a % b =", a % b)
+# print("a ** b =", a ** b)
+# zad3
+# i = 0
+# while i <= 10:
+#     i += 1
+# print(i)
+# while i > 7:
+#     i -= 1
+# print(i)
+# while i >= 2:
+#     i = i / 2
+# print(i)
+# while i < 20:
+#     i = i * 4
+# print(i)
+# while i > 0.5:
+#     i = i % 2
+# print(i)
+# i = 2
+# while i < 64:
+#     i = i ** 2
+# print(i)
+# zad4
+# a = m.exp(10)
+# print(a)
+# b = m.sin(8) ** 2
+# b = m.log(5+b)
+# b = b**(1/6)
+# print(b)
+# c = 3.55
+# c = int(c)
+# print(c)
+# d = 4.8
+# d = int(d) + 1
+# print(d)
+# zad 5
+# imie = input("Podaj imie:")
+# naz = input("Podaj nazwisko: ")
+# imie = imie.capitalize()
+# naz = naz.capitalize()
+# print(imie)
+# print(naz)
+# zad 6
+# x = "Oh-oh-oh-oh-oooh Oh-oh-oh-oh-oooh, oh-oh-oh Caught in a bad romance Oh-oh-oh-oh-oooh Oh-oh-oh-oh-oooh, " \
+#     "oh-oh-oh Caught in a bad romance Rah, rah-ah-ah-ah Roma, roma-ma Gaga, ooh-la-la Want your bad romance"
+# y = x.count("oh")
+# print(y)
+# zad 7
+# imie = input("Podaj imie: ")
+# print(imie[1])
+# print(imie[-1])
+# zad 8
+# y = x.split(" ")
+# print(y)
+# zad 9
+# x = "string"
+# y = 5.44
+# z = hex(20)
+# print(x)
+# print(y)
+# print(z)
