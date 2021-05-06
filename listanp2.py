3bb34a07165d480a9ba66ecfa94f7cabe3bb30a7
Wojciech <kojtek41@wp.pl>
2021-05-06 18:01:06 +0200

    Druga lista numpy

---
+++ listanp2.py
@@ -0,0 +1,51 @@
+import numpy as np
+import math as m
+# zad1
+# a=np.arange(3)
+# b=np.arange(3)
+# b = b*2
+# c = a.dot(b)
+# print(c)
+# zad3
+# c = a*b
+# print(c)
+# zad4
+# a=np.arange(3)
+# b=np.arange(3)
+# b = b*0.3
+# c=a.dot(b)
+# print(c)
+# zad5
+# tab = np.arange(6).reshape(2,3)
+# a = np.sin(tab)
+# print(a)
+# zad6
+# tab2 = np.arange(6).reshape(2,3)
+# b = np.cos(tab)
+# print(b)
+# zad7
+# c = a+b
+# print(c)
+# zad8
+# a = np.arange(9).reshape(3,3)
+# for x in a:
+#     print(x)
+#     print(" ")
+# zad9
+# a = np.arange(9).reshape(3,3)
+# for x in a.flat:
+#     print(x)
+# zad 11
+# a = np.arange(12)
+# b = a.reshape(3,4)
+# c = a.reshape(4,3)
+# d = a.reshape(2,6)
+# print(b)
+# b=b.ravel()
+# print(b)
+# print(c)
+# c=c.ravel()
+# print(c)
+# print(d)
+# d=d.ravel()
+# print(d)
